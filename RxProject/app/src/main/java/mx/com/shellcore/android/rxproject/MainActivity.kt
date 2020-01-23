88910ef45d310e5a8ed563212639c670d37e1e9a
Cesar Morales Garduño <shell.one.core@gmail.com>
2020-01-23 16:04:22 -0600

    Creación de ejemplo para subscripción básica a observable en el hilo main

--- RxProject/app/src/main/java/mx/com/shellcore/android/rxproject/MainActivity.kt
+++ RxProject/app/src/main/java/mx/com/shellcore/android/rxproject/MainActivity.kt
@@ -1,12 +1,22 @@
 package mx.com.shellcore.android.rxproject
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        setupOnclicks()
+    }
+
+    private fun setupOnclicks() {
+        btnRx00Intro.setOnClickListener {
+            startActivity(Intent(this, Rx00IntroActivity::class.java))
+        }
     }
 }
