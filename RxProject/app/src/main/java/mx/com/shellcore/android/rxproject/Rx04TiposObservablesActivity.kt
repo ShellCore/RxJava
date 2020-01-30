c4bbc4d3a473079a090e5cd524dd6eab191a3302
Cesar Morales Garduño <shell.one.core@gmail.com>
2020-01-30 08:26:35 -0600

    Implementación de los tipos de observables Single y SingleObserver

--- RxProject/app/src/main/java/mx/com/shellcore/android/rxproject/Rx04TiposObservablesActivity.kt
+++ RxProject/app/src/main/java/mx/com/shellcore/android/rxproject/Rx04TiposObservablesActivity.kt
@@ -5,6 +5,8 @@ import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import io.reactivex.Observable
 import io.reactivex.Observer
+import io.reactivex.Single
+import io.reactivex.SingleObserver
 import io.reactivex.disposables.Disposable
 
 class Rx04TiposObservablesActivity : AppCompatActivity() {
@@ -13,7 +15,8 @@ class Rx04TiposObservablesActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_rx04_tipos_observables)
 
-        observableObserver()
+//        observableObserver()
+        singleSingleObserver()
     }
 
     private fun observableObserver() {
@@ -43,6 +46,29 @@ class Rx04TiposObservablesActivity : AppCompatActivity() {
         }
 
         observable.subscribe(observer)
+    }
+
+    private fun singleSingleObserver() {
+        "SINGLE - SINGLEOBSERVER".showTitle()
+
+        val single = Single.create<Empleado> {
+            val empleado = Empleado(1, "Cesar Morales", "Developer", "2019-12-18", 25000.0, 10000.0)
+            it.onSuccess(empleado)
+        }
+
+        val singleObserver = object : SingleObserver<Empleado> {
+            override fun onSuccess(t: Empleado) {
+                "onSuccess: ${t.nombre}".showLog()
+            }
+
+            override fun onSubscribe(d: Disposable) {
+            }
+
+            override fun onError(e: Throwable) {
+            }
+        }
+
+        single.subscribe(singleObserver)
     }
 }
 
