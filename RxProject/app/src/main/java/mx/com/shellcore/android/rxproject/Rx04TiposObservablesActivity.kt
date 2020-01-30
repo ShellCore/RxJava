09b27097f9102d53f3425b6e0ed75cf78fe92853
Cesar Morales Garduño <shell.one.core@gmail.com>
2020-01-30 08:16:27 -0600

    Implementación de los tipos de observables Observable y Observer

--- RxProject/app/src/main/java/mx/com/shellcore/android/rxproject/Rx04TiposObservablesActivity.kt
+++ RxProject/app/src/main/java/mx/com/shellcore/android/rxproject/Rx04TiposObservablesActivity.kt
@@ -1,12 +1,55 @@
 package mx.com.shellcore.android.rxproject
 
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import io.reactivex.Observable
+import io.reactivex.Observer
+import io.reactivex.disposables.Disposable
 
 class Rx04TiposObservablesActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_rx04_tipos_observables)
+
+        observableObserver()
     }
+
+    private fun observableObserver() {
+        "OBSERVABLE - OBSERVER".showTitle()
+        val empleados = Empleado.getListaEmpleados()
+        val observable = Observable.create<Empleado> {
+            for (empleado in empleados) {
+                it.onNext(empleado)
+            }
+            it.onComplete()
+        }
+
+        val observer = object : Observer<Empleado> {
+            override fun onComplete() {
+                "onComplete".showLog()
+            }
+
+            override fun onSubscribe(d: Disposable) {
+            }
+
+            override fun onNext(t: Empleado) {
+                "onNext: ${t.nombre}".showLog()
+            }
+
+            override fun onError(e: Throwable) {
+            }
+        }
+
+        observable.subscribe(observer)
+    }
+}
+
+private fun String.showLog() {
+    Log.d("Rx4Observables", this)
+}
+
+private fun String.showTitle() {
+    "------------------------------ $this ------------------------------".showLog()
 }
