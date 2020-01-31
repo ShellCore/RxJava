28e151830f3ea6450ad7230c0d829da5eb336ca3
Cesar Morales Garduño <shell.one.core@gmail.com>
2020-01-31 12:00:01 -0600

    Implementación del tipo de Subject PublishSubject

--- RxProject/app/src/main/java/mx/com/shellcore/android/rxproject/Rx05SubjectActivity.kt
+++ RxProject/app/src/main/java/mx/com/shellcore/android/rxproject/Rx05SubjectActivity.kt
@@ -2,11 +2,60 @@ package mx.com.shellcore.android.rxproject
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import io.reactivex.Observer
+import io.reactivex.disposables.Disposable
+import io.reactivex.subjects.PublishSubject
 
 class Rx05SubjectActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_rx05_subject)
+
+        probePublishSubject()
+    }
+
+    private fun probePublishSubject() {
+        "PUBLISH_SUBJECT".showTitle()
+        val source = PublishSubject.create<String>()
+        val obs = object : Observer<String> {
+            override fun onComplete() {
+                "Primer observer. onComplete".showLog()
+            }
+
+            override fun onSubscribe(d: Disposable) {}
+
+            override fun onNext(t: String) {
+                "Primer observer. onNext: $t".showLog()
+            }
+
+            override fun onError(e: Throwable) {}
+        }
+
+        val obs2 = object : Observer<String> {
+            override fun onComplete() {
+                "Segundo observer. onComplete".showLog()
+            }
+
+            override fun onSubscribe(d: Disposable) {}
+
+            override fun onNext(t: String) {
+                "Segundo observer. onNext: $t".showLog()
+            }
+
+            override fun onError(e: Throwable) {}
+        }
+
+        source.apply {
+            subscribe(obs)
+            onNext("A")
+            onNext("B")
+            onNext("C")
+            subscribe(obs2)
+            onNext("1")
+            onNext("2")
+            onNext("3")
+            onComplete()
+        }
     }
 }
