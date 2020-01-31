dbfe657b31e29e89ddfd784837c92f4dbc6b007d
Cesar Morales Garduño <shell.one.core@gmail.com>
2020-01-31 12:09:18 -0600

    Implementación del tipo de Subject ReplySubject

--- RxProject/app/src/main/java/mx/com/shellcore/android/rxproject/Rx05SubjectActivity.kt
+++ RxProject/app/src/main/java/mx/com/shellcore/android/rxproject/Rx05SubjectActivity.kt
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity
 import io.reactivex.Observer
 import io.reactivex.disposables.Disposable
 import io.reactivex.subjects.PublishSubject
+import io.reactivex.subjects.ReplaySubject
 
 class Rx05SubjectActivity : AppCompatActivity() {
 
@@ -12,12 +13,59 @@ class Rx05SubjectActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_rx05_subject)
 
-        probePublishSubject()
+//        probePublishSubject()
+        probeReplySubject()
     }
 
     private fun probePublishSubject() {
         "PUBLISH_SUBJECT".showTitle()
         val source = PublishSubject.create<String>()
+        val obs = object : Observer<String> {
+            override fun onComplete() {
+                "Primer observer. onComplete".showLog()
+            }
+
+            override fun onSubscribe(d: Disposable) {}
+
+            override fun onNext(t: String) {
+                "Primer observer. onNext: $t".showLog()
+            }
+
+            override fun onError(e: Throwable) {}
+        }
+
+        val obs2 = object : Observer<String> {
+            override fun onComplete() {
+                "Segundo observer. onComplete".showLog()
+            }
+
+            override fun onSubscribe(d: Disposable) {}
+
+            override fun onNext(t: String) {
+                "Segundo observer. onNext: $t".showLog()
+            }
+
+            override fun onError(e: Throwable) {}
+        }
+
+        source.apply {
+            subscribe(obs)
+            onNext("A")
+            onNext("B")
+            onNext("C")
+            subscribe(obs2)
+            onNext("1")
+            onNext("2")
+            onNext("3")
+            onComplete()
+        }
+    }
+
+    private fun probeReplySubject() {
+        "REPLY_SUBJECT".showTitle()
+
+        val source = ReplaySubject.create<String>()
+
         val obs = object : Observer<String> {
             override fun onComplete() {
                 "Primer observer. onComplete".showLog()
