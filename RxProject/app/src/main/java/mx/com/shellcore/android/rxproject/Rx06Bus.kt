4d853ac8240879254d6131062e89c1001ae779c0
Cesar Morales Garduño <shell.one.core@gmail.com>
2020-02-04 12:31:24 -0600

    Creación de bus

--- RxProject/app/src/main/java/mx/com/shellcore/android/rxproject/Rx06Bus.kt
+++ RxProject/app/src/main/java/mx/com/shellcore/android/rxproject/Rx06Bus.kt
@@ -1,5 +1,27 @@
 package mx.com.shellcore.android.rxproject
 
+import io.reactivex.Observable
+import io.reactivex.subjects.PublishSubject
+
 class Rx06Bus {
 
+    companion object {
+        private var instance: Rx06Bus? = null
+
+        fun getInstance() {
+            if (instance == null) {
+                instance = Rx06Bus()
+            }
+        }
+    }
+
+    private var bus: PublishSubject<Any> = PublishSubject.create()
+
+    fun setEvents(message: Any) {
+        bus.onNext(message)
+    }
+
+    fun getEvents(): Observable<Any> {
+        return bus
+    }
 }
