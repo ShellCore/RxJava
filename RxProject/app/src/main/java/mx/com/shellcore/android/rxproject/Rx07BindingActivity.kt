b172f8df77101d00e97893c1c2ab2c2f6fdf396e
Cesar Morales Garduño <shell.one.core@gmail.com>
2020-02-04 13:23:27 -0600

    Ejemplo de RxBinding para click sencillo en botones de la vista

--- RxProject/app/src/main/java/mx/com/shellcore/android/rxproject/Rx07BindingActivity.kt
+++ RxProject/app/src/main/java/mx/com/shellcore/android/rxproject/Rx07BindingActivity.kt
@@ -2,11 +2,29 @@ package mx.com.shellcore.android.rxproject
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.jakewharton.rxbinding3.view.clicks
+import io.reactivex.disposables.Disposable
+import kotlinx.android.synthetic.main.activity_rx07_binding.*
 
 class Rx07BindingActivity : AppCompatActivity() {
+
+    private lateinit var disposable: Disposable
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_rx07_binding)
+
+        btnRxBinding.setOnClickListener {
+            "onClick normal".showLog()
+        }
+
+        disposable = btnRxBinding2.clicks().subscribe {
+            "onClick utilizando Rx".showLog()
+        }
+    }
+
+    override fun onDestroy() {
+        disposable.dispose()
+        super.onDestroy()
     }
 }
