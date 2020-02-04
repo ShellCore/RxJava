0bd6a98ceb7b1ae2900f1cba7044fecba411e9d9
Cesar Morales Garduño <shell.one.core@gmail.com>
2020-02-04 15:50:00 -0600

    Generación de estrategia Buffer para resolver el problema del Back Pressure

--- RxProject/app/src/main/java/mx/com/shellcore/android/rxproject/Rx08BackPressureActivity.kt
+++ RxProject/app/src/main/java/mx/com/shellcore/android/rxproject/Rx08BackPressureActivity.kt
@@ -2,8 +2,10 @@ package mx.com.shellcore.android.rxproject
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import io.reactivex.Flowable
 import io.reactivex.schedulers.Schedulers
 import io.reactivex.subjects.PublishSubject
+import java.util.concurrent.TimeUnit
 
 class Rx08BackPressureActivity : AppCompatActivity() {
 
@@ -11,26 +13,34 @@ class Rx08BackPressureActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_rx08_back_pressure)
 
-        generarBackPressure()
+//        generarBackPressure()
+        backPressureBuffer()
     }
 
     private fun generarBackPressure() {
         val source = PublishSubject.create<Int>()
         val disposable = source.observeOn(Schedulers.io())
-            .subscribe({
-                largeDurationOperation(it)
-            }, {
-                "onError: ${it.localizedMessage}".showLog()
-            }, {
-                "onComplete".showLog()
-            })
+            .subscribe({ largeDurationOperation(it) },
+                { "onError: ${it.localizedMessage}".showLog() },
+                { "onComplete".showLog() })
 
         for (i in 0..10) {
             source.onNext(i)
             "Creando item observable $i".showLog()
         }
-
         source.onComplete()
+    }
+
+    private fun backPressureBuffer() {
+        val source = Flowable.interval(1, TimeUnit.MILLISECONDS)
+        val disposable = source.onBackpressureBuffer(1000)
+            .observeOn(Schedulers.computation())
+            .subscribe({
+                "Consumiendo observables $it".showLog()
+                Thread.sleep(100)
+            }, {
+                "onError: ${it.localizedMessage}".showLog()
+            })
     }
 
     private fun largeDurationOperation(number: Int) {
