af61eb2a63c1d0339e860f754c063d2da154ee24
Cesar Morales Garduño <shell.one.core@gmail.com>
2020-02-04 12:57:03 -0600

    Finalizando el bus

--- RxProject/app/src/main/java/mx/com/shellcore/android/rxproject/Rx06BusFragment.kt
+++ RxProject/app/src/main/java/mx/com/shellcore/android/rxproject/Rx06BusFragment.kt
@@ -1,24 +1,40 @@
 package mx.com.shellcore.android.rxproject
 
-
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import io.reactivex.disposables.CompositeDisposable
+import kotlinx.android.synthetic.main.fragment_rx06_bus.*
 
-/**
- * A simple [Fragment] subclass.
- */
 class Rx06BusFragment : Fragment() {
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_rx06_bus, container, false)
+    private val compositeDisposable by lazy {
+        CompositeDisposable()
     }
 
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view = inflater.inflate(R.layout.fragment_rx06_bus, container, false)
+        configFragment()
+        return view
+    }
+
+    private fun configFragment() {
+        val observable = Rx06Bus.getInstance()
+            .getEvents()
+        compositeDisposable.add(observable.subscribe {
+            txtFragment.text = "$it"
+        })
+    }
+
+    override fun onDestroy() {
+        compositeDisposable.clear()
+        super.onDestroy()
+    }
 
 }
