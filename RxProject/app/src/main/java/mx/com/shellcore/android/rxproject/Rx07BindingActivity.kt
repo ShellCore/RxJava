130c2975b30620d03df32b1ef2cb0a1266783ac1
Cesar Morales Garduño <shell.one.core@gmail.com>
2020-02-04 13:31:44 -0600

    Ejemplo de RxBinding para observación de cambios en el texto de un editText

--- RxProject/app/src/main/java/mx/com/shellcore/android/rxproject/Rx07BindingActivity.kt
+++ RxProject/app/src/main/java/mx/com/shellcore/android/rxproject/Rx07BindingActivity.kt
@@ -3,12 +3,15 @@ package mx.com.shellcore.android.rxproject
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.jakewharton.rxbinding3.view.clicks
-import io.reactivex.disposables.Disposable
+import com.jakewharton.rxbinding3.widget.textChanges
+import io.reactivex.disposables.CompositeDisposable
 import kotlinx.android.synthetic.main.activity_rx07_binding.*
 
 class Rx07BindingActivity : AppCompatActivity() {
 
-    private lateinit var disposable: Disposable
+    private val compositeDisposable by lazy {
+        CompositeDisposable()
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,13 +21,29 @@ class Rx07BindingActivity : AppCompatActivity() {
             "onClick normal".showLog()
         }
 
-        disposable = btnRxBinding2.clicks().subscribe {
+        compositeDisposable.add(btnRxBinding2.clicks().subscribe {
             "onClick utilizando Rx".showLog()
-        }
+        })
+
+        // Forma antigua para la implementación de un escuchador de cambios en un campo de texto
+        /*tilRxBinding.editText!!.addTextChangedListener(object : TextWatcher {
+            override fun afterTextChanged(s: Editable?) {
+            }
+
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+            }
+        })*/
+        compositeDisposable.addAll(tilRxBinding.editText!!.textChanges()
+            .subscribe {
+                "onTextChanged: $it".showLog()
+            })
     }
 
     override fun onDestroy() {
-        disposable.dispose()
+        compositeDisposable.clear()
         super.onDestroy()
     }
 }
