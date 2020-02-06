35a7389f3ab012f84b2e984d76570656b5bfc873
Cesar Morales Garduño <shell.one.core@gmail.com>
2020-02-06 08:12:59 -0600

    Implementación de consulta de servicio web, sin la utilización de Rx.

--- RxRetrofit/app/src/main/java/mx/com/shellcore/android/rxretrofit/RxRetrofitActivity.kt
+++ RxRetrofit/app/src/main/java/mx/com/shellcore/android/rxretrofit/RxRetrofitActivity.kt
@@ -2,11 +2,16 @@ package mx.com.shellcore.android.rxretrofit
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import androidx.recyclerview.widget.LinearLayoutManager
 import io.reactivex.disposables.CompositeDisposable
 import kotlinx.android.synthetic.main.activity_rx_retrofit.*
 import mx.com.shellcore.android.rxretrofit.adapter.RepositoryAdapter
+import mx.com.shellcore.android.rxretrofit.api.WebService
 import mx.com.shellcore.android.rxretrofit.model.GithubRepo
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 import java.util.ArrayList
 
 class RxRetrofitActivity : AppCompatActivity() {
@@ -20,6 +25,7 @@ class RxRetrofitActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_rx_retrofit)
         setupView()
+        getListWidthoutRx()
     }
 
     private fun setupView() {
@@ -27,5 +33,25 @@ class RxRetrofitActivity : AppCompatActivity() {
             layoutManager = LinearLayoutManager(this@RxRetrofitActivity, LinearLayoutManager.VERTICAL, false)
             adapter = reposAdapter
         }
+    }
+
+    private fun getListWidthoutRx() {
+        val call = WebService.getInstance()
+            .createService()
+            .getReposForUser("JakeWharton")
+
+        call.enqueue(object : Callback<List<GithubRepo>> {
+            override fun onResponse(
+                call: Call<List<GithubRepo>>,
+                response: Response<List<GithubRepo>>
+            ) {
+                githubRepos = response.body()!!
+                reposAdapter.setRepos(githubRepos)
+            }
+
+            override fun onFailure(call: Call<List<GithubRepo>>, t: Throwable) {
+                Log.e("GetReposForUser", t.localizedMessage!!)
+            }
+        })
     }
 }
--- RxRetrofit/app/src/main/java/mx/com/shellcore/android/rxretrofit/api/WebServiceApi.kt
+++ RxRetrofit/app/src/main/java/mx/com/shellcore/android/rxretrofit/api/WebServiceApi.kt
@@ -2,11 +2,15 @@ package mx.com.shellcore.android.rxretrofit.api
 
 import io.reactivex.Observable
 import mx.com.shellcore.android.rxretrofit.model.GithubRepo
+import retrofit2.Call
 import retrofit2.http.GET
 import retrofit2.http.Path
 
 interface WebServiceApi {
 
     @GET("users/{user}/repos")
-    fun getReposForUser(@Path("user") user: String): Observable<List<GithubRepo>>
+    fun getReposForUser(@Path("user") user: String): Call<List<GithubRepo>>
+
+    @GET("users/{user}/repos")
+    fun getReposForUserRx(@Path("user") user: String): Observable<List<GithubRepo>>
 }
