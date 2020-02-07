5100d0654f40c597a9c93ca8ac52663da4b5b17a
Cesar Morales Garduño <shell.one.core@gmail.com>
2020-02-07 07:56:22 -0600

    Creación de Activity para consumo de servicios anidado

---
+++ RxRetrofit/app/src/main/java/mx/com/shellcore/android/rxretrofit/RxRetrofitAnidadoActivity.kt
@@ -0,0 +1,12 @@
+package mx.com.shellcore.android.rxretrofit
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class RxRetrofitAnidadoActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_rx_retrofit_anidado2)
+    }
+}
