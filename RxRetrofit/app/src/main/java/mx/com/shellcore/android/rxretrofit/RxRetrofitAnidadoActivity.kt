f547441d9bbb94171780c3fa2df705b7f7b61629
Cesar Morales Garduño <shell.one.core@gmail.com>
2020-02-07 09:24:18 -0600

    Creación de la petición para la consulta de contribuidores

--- RxRetrofit/app/src/main/java/mx/com/shellcore/android/rxretrofit/RxRetrofitAnidadoActivity.kt
+++ RxRetrofit/app/src/main/java/mx/com/shellcore/android/rxretrofit/RxRetrofitAnidadoActivity.kt
@@ -2,12 +2,23 @@ package mx.com.shellcore.android.rxretrofit
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import io.reactivex.Observable
+import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.disposables.CompositeDisposable
+import io.reactivex.schedulers.Schedulers
+import kotlinx.android.synthetic.main.activity_rx_retrofit_anidado.*
 import mx.com.shellcore.android.rxretrofit.adapter.ContributorsAdapter
+import mx.com.shellcore.android.rxretrofit.api.WebService
 import mx.com.shellcore.android.rxretrofit.model.Contributor
-import java.util.ArrayList
+import java.util.*
+import kotlin.collections.ArrayList
 
 class RxRetrofitAnidadoActivity : AppCompatActivity() {
+
+    companion object {
+        private const val USER = "JakeWharton"
+    }
 
     private val compositeDisposable by lazy {
         CompositeDisposable()
@@ -25,7 +36,39 @@ class RxRetrofitAnidadoActivity : AppCompatActivity() {
         setupView()
     }
 
+    override fun onDestroy() {
+        compositeDisposable.clear()
+        super.onDestroy()
+    }
+
     private fun setupView() {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        recContributors.apply {
+            layoutManager = LinearLayoutManager(this@RxRetrofitAnidadoActivity, LinearLayoutManager.VERTICAL, false)
+            adapter = contributorsAdapter
+        }
+    }
+
+    private fun peticionesAnidadasServidorRx() {
+        compositeDisposable.add(WebService.getInstance()
+            .createService()
+            .getReposForUserRx(USER)
+            .subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
+            .toObservable()
+            .flatMap {
+                Observable.fromIterable(it)
+            }.flatMap {
+                WebService.getInstance()
+                    .createService()
+                    .getRepoContributorsForUserAndRepoRx(USER, it.name)
+                    .subscribeOn(Schedulers.io())
+            }.flatMap {
+                Observable.fromIterable(it)
+            }.observeOn(AndroidSchedulers.mainThread())
+            .subscribe {
+                (contributors as ArrayList).add(it)
+                contributorsAdapter.setContributors(contributors)
+            }
+        )
     }
 }
